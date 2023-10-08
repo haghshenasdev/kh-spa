2402f9c6f1e81c0428a30351a83047dcec130a31
haghshenas <mhgorgab@gmail.com>
2023-10-08 22:11:48 +0330

    added charity id changeable , ...

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 import {authHeader} from '@/composable/useApi'
-console.log(document.getElementById('chid'))
-  axios.defaults.baseURL = 'https://khapp.kheiriehemamali.ir/api/v1/1';
+var chidElement = document.getElementById('chid');
+let charityID = chidElement.value;
+chidElement.remove()
+  axios.defaults.baseURL = 'https://khapp.kheiriehemamali.ir/api/v1/' + charityID;
   // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1/1';
   axios.defaults.headers = {
     'Access-Control-Allow-Origin': '*',
