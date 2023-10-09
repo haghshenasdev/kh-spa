d7bc6d222b84c3d66641ac8b68fe61e17b829fa0
haghshenas <mhgorgab@gmail.com>
2023-10-09 23:28:29 +0330

    bug fixed in register for optional email

--- src/classes/AuthServices.js
+++ src/classes/AuthServices.js
@@ -1,50 +1,51 @@
 import axios from "@/plugins/axios";
-import {getFormDataBody} from '@/composable/useApi'
+import { getFormDataBody } from '@/composable/useApi'
 
 class AuthServices {
 
-  async login(user){
+  async login(user) {
 
     return axios
-        .post('login', getFormDataBody({
-          phone: user.phone,
-          password: user.password,
-          device_name: 'web',
-        }))
-        .then(response => {
-          // console.log(response.data.status)
-          if (response.data.status == 'success' && response.data.data.token) {
-            localStorage.setItem('user', JSON.stringify(response.data.data));
-          }
-          return response.data;
-        });
+      .post('login', getFormDataBody({
+        phone: user.phone,
+        password: user.password,
+        device_name: 'web',
+      }))
+      .then(response => {
+        // console.log(response.data.status)
+        if (response.data.status == 'success' && response.data.data.token) {
+          localStorage.setItem('user', JSON.stringify(response.data.data));
+        }
+        return response.data;
+      });
   }
 
-  async register(user){
+  async register(user) {
+    var body = {
+      phone: user.phone,
+      name: user.name,
+      password: user.password,
+      password_confirmation: user.password_confirmation,
+      device_name: 'web',
+    };
+    if (user.email != null) body.email = user.email;
 
     return axios
-        .post('register', getFormDataBody({
-          phone: user.phone,
-          email: user.email,
-          name: user.name,
-          password: user.password,
-          password_confirmation: user.password_confirmation,
-          device_name: 'web',
-        }))
-        .then(response => {
-          // console.log(response.data.status)
-          if (response.data.status == 'success' && response.data.data.token) {
-            localStorage.setItem('user', JSON.stringify(response.data.data));
-          }
-          return response.data;
-        });
+      .post('register', getFormDataBody(body))
+      .then(response => {
+        // console.log(response.data.status)
+        if (response.data.status == 'success' && response.data.data.token) {
+          localStorage.setItem('user', JSON.stringify(response.data.data));
+        }
+        return response.data;
+      });
   }
 
   logout() {
     localStorage.removeItem('user');
   }
 
-  checkAuth(){
+  checkAuth() {
     let user = JSON.parse(localStorage.getItem('user'));
     console.log(user)
     if (user && user.token) return true;
